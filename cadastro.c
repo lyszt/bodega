65400a61186cce01c94160bb266fde1fc7efcb98
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-08 19:05:26 -0300

    removed verifications

--- cadastro.c
+++ cadastro.c
@@ -12,10 +12,9 @@ void cadastrarBebida(Sentinela *sentinela) {
         return;
     }
 
-    while(nova->codigo <= 0){
-        printf("Código: ");
-        scanf("%d", &nova->codigo);
-    }
+    printf("Código: ");
+    scanf("%d", &nova->codigo);
+
     Bebida *atual = sentinela->head;
     while (atual) {
         if (atual->codigo == nova->codigo) {
@@ -26,38 +25,21 @@ void cadastrarBebida(Sentinela *sentinela) {
         atual = atual->next;
     }
 
-    bool is_valid = false;
-    while(!is_valid){
-        printf("Nome: ");
-        scanf("%s", nova->nome);
-        is_valid = true; // Assume name is valid initially
-        for(int i = 0; i < strlen(nova->nome); i++) {
-            if ((nova->nome[i] >= 65 && nova->nome[i] <= 90) || (nova->nome[i] >= 97 && nova->nome[i] <= 122)) {
-                // Valid character (alphabet)
-                continue;
-            } else {
-                is_valid = false; // Invalid character found
-                break;
-            }
-        }
-    }
+    printf("Nome: ");
+    scanf("%s", nova->nome);
 
-    while(nova->volume <= 0){
-        printf("Volume: ");
-        scanf("%f", &nova->volume);
-    }
-    while(nova->preco <= 0){
-        printf("Preço: ");
-        scanf("%f", &nova->preco);
-    }
-    while(nova->quantidade <= 0){
-        printf("Quantidade em estoque: ");
-        scanf("%d", &nova->quantidade);
-    }
-    while(nova->teorAlcoolico != 1 && nova->teorAlcoolico != 0){
-        printf("Teor alcoólico (0 = Não alcoólica, 1 = Alcoólica): ");
-        scanf("%d", &nova->teorAlcoolico);
-    }
+    printf("Volume: ");
+    scanf("%f", &nova->volume);
+
+    printf("Preço: ");
+    scanf("%f", &nova->preco);
+
+    printf("Quantidade em estoque: ");
+    scanf("%d", &nova->quantidade);
+
+    printf("Teor alcoólico (0 = Não alcoólica, 1 = Alcoólica): ");
+    scanf("%d", &nova->teorAlcoolico);
+
 
     if (sentinela->head == NULL) {
         sentinela->head = nova;
@@ -92,33 +74,17 @@ void cadastrarCliente(Sentinela *sentinela) {
         atual = atual->next;
     }
 
-    bool is_valid = false;
-    while(!is_valid){
-        printf("Nome: ");
-        scanf("%s", novo->nome);
-        is_valid = true; // Assume name is valid initially
-        for(int i = 0; i < strlen(novo->nome); i++) {
-            if ((novo->nome[i] >= 65 && novo->nome[i] <= 90) || (novo->nome[i] >= 97 && novo->nome[i] <= 122)) {
-                // Valid character (alphabet)
-                continue;
-            } else {
-                is_valid = false; // Invalid character found
-                break;
-            }
-        }
-    }
-
+    printf("Nome: ");
+    scanf("%s", novo->nome);
     printf("CPF do cliente: ");
     scanf("%12s", novo->cpf);
 
-    while(novo->idade <= 0){
-        printf("Idade do cliente: ");
-        scanf("%d", &novo->idade);
-    }
-    while(novo->vendeFiado != 0 && novo->vendeFiado != 1){
-        printf("Vende fiado (0 = Não, 1 = Sim): ");
-        scanf("%d", &novo->vendeFiado);
-    }
+    printf("Idade do cliente: ");
+    scanf("%d", &novo->idade);
+
+    printf("Vende fiado (0 = Não, 1 = Sim): ");
+    scanf("%d", &novo->vendeFiado);
+
     if (sentinela->headCliente == NULL) {
         sentinela->headCliente = novo;
     } else {
